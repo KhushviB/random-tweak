5f17d7efb509d1e393f1594b48ab81329e5aff1a
Khushvi Bamrolia <khushvi.bamrolia28@gmail.com>
2025-04-29 01:07:05 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ async fn main() -> std::io::Result<()> {
         .service(Files::new("/", "frontend").index_file("index.html"))
 })
 
-    .bind("127.0.0.1:8080")?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
